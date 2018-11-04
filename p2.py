286afac4ca88da2a925a0c33a2527a41bebe683c
Venkatesh <kamath797@gmail.com>
2018-11-05 01:41:25 +0530

    Accuracy using LinearRegression

--- p2.py
+++ p2.py
@@ -37,7 +37,7 @@ y = np.array(df['label'])
 # print(len(X),len(y))
 X_train, X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
 
-classifier = svm.SVR(kernel='poly')
+classifier = LinearRegression()
 classifier.fit(X_train,y_train)
 accuracy = classifier.score(X_test,y_test)
 
