5c7b480bff92c5aafd53c82bfe295720135dca1d
Venkatesh <kamath797@gmail.com>
2018-11-06 00:27:24 +0530

    Getting the statistics mean using mean function for regression slope

---
+++ BestFitSlope.py
@@ -0,0 +1,28 @@
+# slope formulae
+# m =  (x' *  y'  - (x*y)')
+#      ----------------------
+#       ((x')^2) - ((x^2)')
+# " ' represents mean" 
+
+import statistics 
+import numpy as np
+import matplotlib.pyplot as plt 
+
+xs = np.array([1,2,3,4,5,6],dtype=np.float64)
+ys = np.array([5,4,6,5,6,7],dtype=np.float64)
+
+def best_fit_slope(xs,ys) :
+    x_compliment= statistics.mean(xs)
+    y_compliment = statistics.mean(ys)
+    x_into_y_compliment = statistics.mean(xs*ys)
+    square_of_compliment = x_compliment*x_compliment
+    compliment_of_square = statistics.mean(xs*xs)
+
+    result = ((x_compliment*y_compliment)-x_into_y_compliment)/(square_of_compliment-compliment_of_square)
+    # m = (( (statistics.mean(xs) * statistics.mean(ys)) - statistics.mean(xs*ys)) / (statistics.mean(xs)*statistics.mean(xs)) - statistics.mean(xs*xs))
+    return result
+print(xs.dtype)
+print(ys.dtype)
+
+hello = best_fit_slope(xs,ys)
+print(hello)
